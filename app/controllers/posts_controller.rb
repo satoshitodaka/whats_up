74061705d73d29c959690a091abcf2e9893f9158
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-04 18:04:05 +0900

    [Modify]投稿のindexの並び順を指定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.all.include(:user)
+    @posts = Post.all.include(:user).order(created_at: :desc)
   end
 
   def show
