27347fa196c9d97ee0d9d823322c084c0f5732a6
satoshitodaka <satoshitodaka0705@gmail.com>
2022-06-04 18:10:12 +0900

    [Modify]その他修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 # == Route Map
 #
 #                    Prefix Verb   URI Pattern                                                                              Controller#Action
-#                      root GET    /                                                                                        users#new
-#                     users POST   /users(.:format)                                                                         users#create
-#                  new_user GET    /users/new(.:format)                                                                     users#new
+#                      root GET    /                                                                                        posts#index
 #                     login GET    /login(.:format)                                                                         user_sessions#new
 #                           POST   /login(.:format)                                                                         user_sessions#create
 #                    logout DELETE /logout(.:format)                                                                        user_sessions#destroy
+#                     users POST   /users(.:format)                                                                         users#create
+#                  new_user GET    /users/new(.:format)                                                                     users#new
+#                     posts GET    /posts(.:format)                                                                         posts#index
+#                           POST   /posts(.:format)                                                                         posts#create
+#                  new_post GET    /posts/new(.:format)                                                                     posts#new
+#                 edit_post GET    /posts/:id/edit(.:format)                                                                posts#edit
+#                      post GET    /posts/:id(.:format)                                                                     posts#show
+#                           PATCH  /posts/:id(.:format)                                                                     posts#update
+#                           PUT    /posts/:id(.:format)                                                                     posts#update
+#                           DELETE /posts/:id(.:format)                                                                     posts#destroy
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
@@ -14,13 +22,8 @@
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
-  # ログインしている時のルートパス
-  constraints ->(request) { request.session[:user_id].present? } do
-    root 'posts#index'
-  end
-  # ログインしていない時のルートパス
-  root 'user_sessions#new'
-
+  root 'posts#index'
+  
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
